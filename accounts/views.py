f911896fcc882fb94ca9e4d523c1437aec750538
Mohammad_shop2 <m.rismanbor@gmail.com>
2026-02-28 10:02:05 +0400

    Complate cart2

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from .models import City
 def edit_profile(request):
     return HttpResponse('<h1> edit profile</h1>')
 
+
 def get_cities(request):
     province_id = request.GET.get('province_id')
     if not province_id:
